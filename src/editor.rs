b3f7168de0377702bafb147c3152f779e6015fdd
lapointek <lapointek.dev@gmail.com>
2025-12-14 15:30:22 -0500

    refactor(editor): add match statement

--- src/editor.rs
+++ src/editor.rs
@@ -1,28 +1,30 @@
+use crossterm::event::{Event::Key, KeyCode::Char, read};
 use crossterm::terminal::{disable_raw_mode, enable_raw_mode};
-use std::io::{self, Read};
 
 pub struct Editor {}
-
 impl Editor {
     pub fn default() -> Self {
         Editor {}
     }
     pub fn run(&self) {
         enable_raw_mode().unwrap();
-        for b in io::stdin().bytes() {
-            match b {
-                Ok(b) => {
-                    let c = b as char;
-                    if c.is_control() {
-                        println!("Binary: {0:08b} ASCII: {0:#03} \r", b);
-                    } else {
-                        println!("Binary: {0:08b} ASCII: {0:#03} Character: {1:#?}\r", b, c);
-                    }
-                    if c == 'q' {
-                        break;
+        // continue looping until break
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match (event.code) {
+                        Char(c) => {
+                            if c == 'q' {
+                                break;
+                            }
+                        }
+
+                        _ => (),
                     }
                 }
-                Err(err) => println!("Error: {}", err),
+                Err(err) => println!("Error: {err}"),
+                _ => (),
             }
         }
         disable_raw_mode().unwrap();
